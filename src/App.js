5d606da53627ae121652c9920bb92da77801fdcb
bahram <bahram101@mail.ru>
2022-08-08 18:51:41 +0600

    added Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { About } from './pages/About';
+import { Home } from './pages/Home';
+import { Blog } from './pages/Blog';
+import { NotFound } from './pages/NotFound';
+import './style.scss'
 
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					<Route path="" element={<Home />} />
+					<Route path='blog' element={<Blog />} />
+					<Route path='about' element={<About />} />
+					<Route path='*' element={<NotFound />} />
+				</Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const Header = () => {
+	return (
+		<header>
+			<Container>
+				<Link to='/'>
+					<div className='header__logo'>
+						{/* <img width='38' src={{}} alt='Pizza logo' /> */}
+						<h1>Mel</h1>
+					</div>
+				</Link>
+				<div className='menu'>
+					<Link to="/">
+						<div className='menu-item'>Главная</div>
+					</Link>
+					<Link to="about">
+						<div className='menu-item'>О нас</div>
+					</Link>
+					<Link to="blog">
+						<div className='menu-item'>Блог</div>
+					</Link>
+				</div>
+			</Container>
+		</header>
+	);
+};
