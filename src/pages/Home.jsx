a5e419e519368825ddb4da09e9963fa4bcf2602f
bahram <bahram101@mail.ru>
2022-08-09 18:12:01 +0600

    made header section, added font

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+
+import utik1 from '../assets/images/utik_1.png';
 
 export const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	return (
+		<>
+			<section className='main-block'>
+				<div className='container'>
+					<div className='main-block__left'>
+						<h1>Умная гладильная система из Италии класса люкс</h1>
+            <button >Подробное</button>
+					</div>
+
+					<div className='main-block__right'>
+						<img src={utik1} alt='утюг' />
+					</div>
+				</div>
+			</section>
+			<section className='foot-massager'></section>
+			<section className='back-massager'></section>
+		</>
+	);
+};
