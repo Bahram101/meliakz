e8cfe2923e864be3609dff8f1eb15728726a8074
bahram <bahram101@mail.ru>
2022-08-11 20:02:04 +0600

    added video

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -12,12 +11,4 @@ root.render(
 	</BrowserRouter>
 );
 
-// ReactDOM.render(
-// 	<React.StrictMode>
-// 		<BrowserRouter>
-// 			<App />
-// 		</BrowserRouter>
-// 	</React.StrictMode>,
-// 	document.getElementById('react-root')
-// );
 reportWebVitals();
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,51 +1,21 @@
 import React from 'react';
-import ReactFullpage from '@fullpage/react-fullpage';
+
 import utik1 from '../assets/images/utik_1.png';
+import logo from '../assets/images/logo2.png';
+import video from '../assets/video.mp4'
 
 export const Home = () => {
-	
-	// <ReactFullpage
-	// 	anchors=""
-	// 	navigation
-	// 	navigationTooltips=""
-	// 	navigat
-	// 	sectionsColor={['#7fff00', '#00ffff', '#29ab87']}
-	// 	onLeave={(origin, destination, direction) => {
-	// 		console.log('onLeave event', { origin, destination, direction });
-	// 	}}
-	// 	render={({ state, fullpageApi }) => {
-	// 		console.log('render prop change', state, fullpageApi);
-
-	// 		return (
-	// 			<div>
-	// 				<section className='main-block'>
-	// 					<div className='container'>
-	// 						<div className='main-block__left'>
-	// 							<h1>
-	// 								Умная гладильная система из Италии класса
-	// 								люкс
-	// 							</h1>
-	// 							<button>Подробное</button>
-	// 						</div>
-
-	// 						<div className='main-block__right'>
-	// 							<img src={utik1} alt='утюг' />
-	// 						</div>
-	// 					</div>
-	// 				</section>
-	// 				<section className='foot-massager'></section>
-	// 				<section className='back-massager'></section>
-	// 			</div>
-	// 		);
-	// 	}}
-	// />;
 	return (
 		<>
+			<section className='video-block'>
+				<img src={logo} alt="лого" />
+				<video className="video" src={video} autoPlay loop muted/>
+			</section>
 			<section className='main-block'>
 				<div className='container'>
 					<div className='main-block__left'>
 						<h1>Умная гладильная система из Италии класса люкс</h1>
-	        <button >Подробное</button>
+						<button>Подробное</button>
 					</div>
 
 					<div className='main-block__right'>
@@ -53,7 +23,6 @@ export const Home = () => {
 					</div>
 				</div>
 			</section>
-			<section className='foot-massager'></section>
 			<section className='back-massager'></section>
 		</>
 	);
