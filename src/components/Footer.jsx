d1f192eb6f9d869a12c5745730b6c846ecb7875f
bahram <bahram101@mail.ru>
2022-08-12 18:52:22 +0600

    uploaded images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,88 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {
+	faFacebookF,
+	faInstagram,
+	faWhatsapp,
+} from '@fortawesome/free-brands-svg-icons';
 
 export const Footer = () => {
-  return (
-    <div>Footer - 2022</div>
-  )
-}
+	return (
+		<div className='footer'>
+			<Container>
+				<Row>
+					<Col>
+						<h4>Social websites</h4>
+						<div className='socialIcons'>
+							<ul>
+								<a
+									href='https://www.instagram.com/melia_kazakhstan/?igshid=1jdbrwo3e49sd'
+									target='_blank'
+								>
+									<li>
+										<FontAwesomeIcon
+											icon={faFacebookF}
+											size='lg'
+										/>
+									</li>
+								</a>
+								<a
+									href='https://www.instagram.com/melia_kazakhstan/?igshid=1jdbrwo3e49sd'
+									target='_blank'
+								>
+									<li>
+										<FontAwesomeIcon
+											icon={faInstagram}
+											size='lg'
+										/>
+									</li>
+								</a>
+								<a
+									href='https://web.whatsapp.com/send?phone=77073763659'
+									target='_blank'
+								>
+									<li>
+										<FontAwesomeIcon
+											icon={faWhatsapp}
+											size='lg'
+										/>
+									</li>
+								</a>
+							</ul>
+						</div>
+					</Col>
+					<Col className='contact'>
+						<h4>Contact</h4>
+						<ul>
+							<li>
+								<a href='tel:7071302100'>
+									<FontAwesomeIcon
+										icon={faPhone}
+										size='lg'
+										style={{fontSize: '1.5em'}}
+									/>
+									<span>+7 (707) 300-08-77</span>
+								</a>
+							</li>
+							<li>
+								<a href='mailto:meliaalmaty@gmail.com'>
+                <FontAwesomeIcon
+										icon={faEnvelope}
+										size='lg'
+										style
+									/>
+									<span> meliaalmaty@gmail.com</span>
+								</a>
+							</li>
+						</ul>
+					</Col>
+					<Col>
+						<h4>Address</h4>
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	);
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,61 @@
 import React from 'react';
 
 import utik1 from '../assets/images/utik_1.png';
+import leg1 from '../assets/images/leg1.png';
 import logo from '../assets/images/logo2.png';
-import video from '../assets/video.mp4'
+import video from '../assets/video.mp4';
 
 export const Home = () => {
 	return (
 		<>
-			<section className='video-block'>
-				<img src={logo} alt="лого" />
-				<video className="video" src={video} autoPlay loop muted/>
+			<section className='device video-block'>
+				<img src={logo} alt='лого' />
+				<video className='video' src={video} autoPlay loop muted />
 			</section>
-			<section className='main-block'>
+			<section className='device iron-block'>
 				<div className='container'>
-					<div className='main-block__left'>
-						<h1>Умная гладильная система из Италии класса люкс</h1>
+					<div className='iron-block__left'>
+						<h1>
+							Гладильная система <b>Melia Gold</b>
+						</h1>
+						<p>
+							Это три прибора в одном: утюг, отпариватель для
+							одежды и гладильная доска. Гладильная доска с
+							функциями поддува и отвода пара, сенсорное
+							управление, давление пара – 6 Бар, классический
+							дизайн и оптимальная конструкция (в форме
+							передвижного столика на колесиках) – все это
+							гарантирует профессиональный результат глажения с
+							минимальными усилиями.
+						</p>
 						<button>Подробное</button>
 					</div>
-
-					<div className='main-block__right'>
+					<div className='iron-block__right'>
 						<img src={utik1} alt='утюг' />
 					</div>
 				</div>
 			</section>
-			<section className='back-massager'></section>
+			<section className='device leg-massager-block'>
+				<div className='container'>
+					<div className='leg-massager-block__left'>
+						<img src={leg1} alt='утюг' />
+					</div>
+					<div className='leg-massager-block__right'>
+						<h1>Массажер для ног</h1>
+						<p>
+							Это три прибора в одном: утюг, отпариватель для
+							одежды и гладильная доска. Гладильная доска с
+							функциями поддува и отвода пара, сенсорное
+							управление, давление пара – 6 Бар, классический
+							дизайн и оптимальная конструкция (в форме
+							передвижного столика на колесиках) – все это
+							гарантирует профессиональный результат глажения с
+							минимальными усилиями.
+						</p>
+						<button>Подробное</button>
+					</div>
+				</div>
+			</section>
 		</>
 	);
 };
