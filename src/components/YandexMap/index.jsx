5f9c9b924fd34cc375be08c7295bb11d80b14c6f
bahram <bahram101@mail.ru>
2022-08-25 11:17:15 +0600

    added Yandex API Map

--- src/components/YandexMap/index.jsx
+++ src/components/YandexMap/index.jsx
@@ -1,49 +1,62 @@
-import React from "react";
-import "./map.scss";
-import { Container, Row, Col } from "react-bootstrap";
-import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { YMaps, Map, Placemark } from "react-yandex-maps";
+import React from 'react';
+import './map.scss';
+import { Container, Row, Col } from 'react-bootstrap';
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	YMaps,
+	Map,
+	Placemark,
+	ZoomControl,
+	FullscreenControl,
+} from 'react-yandex-maps';
 
 export const YandexMap = () => {
-  return (
-    <div className="section mapYandex" style={{ height: "100vh" }}>
-      <div className="demo" style={{ height: "42vh" }}>
-        <Container>
-          <Row>
-            <Col md={{ span: 8, offset: 2 }}>
-              <h2>Заявка на бесплатную демонстрацию</h2>
-              <div className="">
-                <div className="demoContacts">
-                  <a href="tel:+77076395949">
-                    <FontAwesomeIcon icon={faPhone} />
-                    <span style={{ marginLeft: "5px" }}>
-                      +7 (707) 639-59-49
-                    </span>
-                  </a>
-                </div>
-                <div>
-                  <a href="mailto:meliaalmaty@gmail.com">
-                    <FontAwesomeIcon icon={faEnvelope} />
-                    <span style={{ marginLeft: "5px" }}>
-                      meliaalmaty@gmail.com
-                    </span>
-                  </a>
-                </div>
-                <div>г. Алматы мамыр 4 дом 5</div>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-      <YMaps>
-        <div>
-          My awesome application with maps!
-          <Map defaultState={{ center: [43.215065, 76.840982], zoom: 6 }} >
-            <Placemark geometry={[43.215065, 76.840982]}/>  
-          </Map>
-        </div>
-      </YMaps>
-    </div>
-  );
+	return (
+		<div className='section mapYandex' style={{ height: '100vh' }}>
+			<div className='demo' style={{ height: '42vh' }}>
+				<Container>
+					<Row>
+						<Col md={{ span: 8, offset: 2 }}>
+							<h2>Заявка на бесплатную демонстрацию</h2>
+							<div className=''>
+								<div className='demoContacts'>
+									<a href='tel:+77076395949'>
+										<FontAwesomeIcon icon={faPhone} />
+										<span style={{ marginLeft: '5px' }}>
+											+7 (707) 639-59-49
+										</span>
+									</a>
+								</div>
+								<div>
+									<a href='mailto:meliaalmaty@gmail.com'>
+										<FontAwesomeIcon icon={faEnvelope} />
+										<span style={{ marginLeft: '5px' }}>
+											meliaalmaty@gmail.com
+										</span>
+									</a>
+								</div>
+								<div>г. Алматы мамыр 4 дом 5</div>
+							</div>
+						</Col>
+					</Row>
+				</Container>
+			</div>
+			<YMaps>
+				<div style={{ height: '58vh' }}>
+					<Map
+						style={{ width: '100wh', height: '100vh' }}
+						defaultState={{
+							center: [43.215065, 76.840982],
+							zoom: 15, 
+						}}
+					>
+						<Placemark geometry={[43.215065, 76.840982]} />
+						<ZoomControl options={{ float: 'left' }} />
+						<FullscreenControl options={{ float: 'left' }} />
+					</Map>
+				</div>
+			</YMaps>
+		</div>
+	);
 };
